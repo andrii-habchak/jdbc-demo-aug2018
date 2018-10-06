b9b80eebbb519768cc450cc572da93424fd3ba59
andrey-gabchak <andrey.gabchak@gmail.com>
2018-10-06 05:56:54 +0300

    created GetCategoryByIdController

---
+++ src/main/java/com/gabchak/controller/GetCategoryByIdController.java
@@ -0,0 +1,27 @@
+package com.gabchak.controller;
+
+import com.gabchak.dao.CategoryDao;
+import com.gabchak.model.Category;
+import com.gabchak.web.Request;
+import com.gabchak.web.ViewModel;
+
+public class GetCategoryByIdController implements Controller {
+    private final CategoryDao categoryDao;
+
+    public GetCategoryByIdController(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @Override
+    public ViewModel process(Request request) {
+        Category category = categoryDao.findById(getIdFromRequest(request));
+        ViewModel vm = ViewModel.of("category");
+        vm.addAttribute("category", category);
+        return vm;
+    }
+
+    private Long getIdFromRequest(Request request) {
+        String iDobject = request.getParamByName("c_id");
+        return Long.valueOf(iDobject);
+    }
+}
