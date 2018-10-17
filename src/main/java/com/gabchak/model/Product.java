76104a3131ab256d206e8ae29cbbd28ec2a2f97d
andrey-gabchak <andrey.gabchak@gmail.com>
2018-10-17 03:19:39 +0300

    Removed redundant code.

--- src/main/java/com/gabchak/model/Product.java
+++ src/main/java/com/gabchak/model/Product.java
@@ -18,7 +18,6 @@ public class Product {
     }
 
     public Product(String name, Double price, String description) {
-        this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
